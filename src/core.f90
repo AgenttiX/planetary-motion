61e4aa62e440cc10990813b7e1718adf947fe2c8
Mika Mäki <agentti002x@gmail.com>
2020-12-17 22:36:02 +0200

    Improve algorithm and GUI
    Now the basic simulation works

--- src/core.f90
+++ src/core.f90
@@ -1,42 +1,46 @@
-subroutine iterate(x, v, a, m, dt, n_objs, n_iters)
-  implicit none
-  ! Double precision is used for NumPy compatibility and more accurate results
-  integer, intent(in) :: n_iters, n_objs
-  real(kind=8), intent(inout) :: x(3,n_objs)
-  real(kind=8), intent(inout) :: v(3,n_objs)
-  real(kind=8), intent(inout) :: a(3,n_objs)
-  real(kind=8), intent(in) :: m(n_objs)
-  real(kind=8), intent(in) :: dt
+module core
+contains
+  function force(x, m, i, n_objs)
+    implicit none
+    integer, intent(in) :: i, n_objs
+    real(kind=8), intent(in) :: x(3,n_objs)
+    real(kind=8), intent(in) :: m(n_objs)
 
-  integer :: i, iter
-  real(kind=8) :: a_prev(3)
+    real(kind=8) :: force(3), dist
+    real(kind=8), parameter :: g = 1, min_dist = 0.01
+    integer :: j
+    force = [0, 0, 0]
 
-  do iter=1,n_iters
-    x = x + v*dt + 0.5*a*dt**2
-    do i=1,n_objs
-      a_prev = a(:,i)
-      ! a(:,i) = F()
-      v(:,i) = v(:,i) + 0.5*(a(:,i) + a_prev)*dt
+    do j=1,n_objs
+      dist = sqrt(sum((x(:,i) - x(:,j))**2))
+      ! Clipping prevents overflow
+      if (dist > min_dist) then
+        force = force + g*m(i)*m(j)*(x(:,j) - x(:,i)) / dist**3
+      end if
     end do
-  end do
-end subroutine iterate
+  end function force
 
-function force(x, m, i, n_objs)
-  implicit none
-  integer, intent(in) :: i, n_objs
-  real(kind=8), intent(in) :: x(3,n_objs)
-  real(kind=8), intent(in) :: m(n_objs)
+  subroutine iterate(x, v, a, m, dt, n_iters, n_objs)
+    ! Note that on Python side the argument n_objs is optional, since it can be automatically determined from the input arrays
+    implicit none
+    ! Double precision is used for NumPy compatibility and more accurate results
+    integer, intent(in) :: n_iters, n_objs
+    real(kind=8), intent(inout) :: x(3,n_objs)
+    real(kind=8), intent(inout) :: v(3,n_objs)
+    real(kind=8), intent(inout) :: a(3,n_objs)
+    real(kind=8), intent(in) :: m(n_objs)
+    real(kind=8), intent(in) :: dt
 
-  real(kind=8) :: force(3)
-  real(kind=8), parameter :: g = 1
-  integer :: j
-  force = [0, 0, 0]
+    integer :: i, iter
+    real(kind=8) :: a_prev(3)
 
-  do j=1,n_objs
-    force = force + g*m(i)*m(j)*(x(:,i) - x(:,j)) / (x(:,i) - x(:,j))**3
-  end do
-end function force
-
-program core
-  print *, "Hello World!"
-end program core
+    do iter=1,n_iters
+      x = x + v*dt + 0.5*a*dt**2
+      do i=1,n_objs
+        a_prev = a(:,i)
+        a(:,i) = force(x, m, i, n_objs) / m(i)
+        v(:,i) = v(:,i) + 0.5*(a(:,i) + a_prev)*dt
+      end do
+    end do
+  end subroutine iterate
+end module core
--- src/main.py
+++ src/main.py
@@ -1,30 +1,75 @@
 #!/usr/bin/env python3
 import subprocess
+import typing as tp
 
 import numpy as np
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+from PySide2 import QtWidgets
 
 # Compilation is done automatically here to speed up development
 print("Compiling Fortran code")
 subprocess.run(["./build.sh"], check=True)
 print("\n\n")
 
-# This line may show errors in an IDE, since the generated Python module is found dynamically
+# Your IDE may complain that the module does not exist, since the generated Python module is found dynamically
 import core
+
+
+class NBodyWidget(gl.GLViewWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("FYS2085 project: Planetary motion")
+        self.pos: tp.Optional[gl.GLScatterPlotItem] = None
+        self.color = (1, 1, 1, .5)
+        self.create_grids()
+
+    def create_grids(self, size: float = 10):
+        size2 = 2*size
+        vec = pg.QtGui.QVector3D(size2, size2, size2)
+        gx = gl.GLGridItem(size=vec)
+        gx.rotate(90, 0, 1, 0)
+        # gx.translate(-10, 0, 0)
+        self.addItem(gx)
+        gy = gl.GLGridItem(size=vec)
+        gy.rotate(90, 1, 0, 0)
+        # gy.translate(0, -10, 0)
+        self.addItem(gy)
+        gz = gl.GLGridItem(size=vec)
+        # gz.translate(0, 0, -10)
+        self.addItem(gz)
+
+    def set_pos(self, pos):
+        if self.pos is None:
+            self.pos = gl.GLScatterPlotItem(pos=pos, color=self.color)
+            self.addItem(self.pos)
+        else:
+            self.pos.setData(pos=pos, color=self.color)
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.nbody = NBodyWidget()
+        self.setCentralWidget(self.nbody)
+        self.resize(1280, 720)
 
 
 def main():
     n_objs = 10
     dt = 0.1
-    n_iters = 10
+    # n_iters = 10
 
-    x = np.asfortranarray(np.random.rand(3, n_objs))
+    x = np.asfortranarray(2*np.random.rand(3, n_objs) - 1) * 10
+    # v = np.asfortranarray(np.random.rand(3, n_objs))
     v = np.zeros_like(x)
     a = np.zeros_like(x)
     m = np.ones(n_objs, order="F")
 
     print("x")
     print(x)
-    core.iterate(x, v, a, m, dt, n_objs, n_iters)
+    print(core.core.iterate.__doc__)
+    # core.core.iterate(x, v, a, m, dt, 1)
 
     print("x")
     print(x)
@@ -32,6 +77,21 @@ def main():
     print(v)
     print("a")
     print(a)
+
+    app = pg.mkQApp()
+    win = MainWindow()
+    win.show()
+    win.nbody.set_pos(x.T)
+
+    def update():
+        core.core.iterate(x, v, a, m, dt, 1)
+        win.nbody.set_pos(x.T)
+
+    timer = pg.QtCore.QTimer()
+    timer.timeout.connect(update)
+    timer.start(100)
+
+    app.exec_()
 
 
 if __name__ == "__main__":
