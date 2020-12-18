f03e25afce5cbce53a9559af5a9f4a1bb104f167
Mika Mäki <agentti002x@gmail.com>
2020-12-18 16:11:03 +0200

    Implement proper file parsing

--- src/main.f90
+++ src/main.f90
@@ -8,12 +8,17 @@ program main
   character(:), allocatable :: path
   real(kind=REAL_KIND), allocatable :: m(:), x(:, :), v(:, :)
   real(kind=REAL_KIND) :: dt
-  integer :: n_steps, n_objs
+  integer :: n_steps, n_objs, print_interval
 
   path = get_path()
-  print *, path
-  call read_file(path, x, v, m, dt, n_steps, n_objs)
+  print *, "Using configuration file: ", path
+  call read_file(path, x, v, m, dt, n_steps, n_objs, print_interval)
+  print *, "n_objs: ", n_objs
+  print *, "n_steps: ", n_steps
+  print *, "dt: ", dt
+  print *, "print_interval: ", print_interval
   call print_arr_2d(x, "x")
   call print_arr_2d(v, "v")
+  call print_arr_1d(m, "m")
 
 end program main
--- src/utils.f90
+++ src/utils.f90
@@ -6,11 +6,21 @@
 ! https://www.geeksforgeeks.org/read-a-file-line-by-line-in-python/
 
 module utils
+  integer, parameter :: DIMS = 3
   integer, parameter :: FILE_UNIT = 10
   integer, parameter :: MAX_LINE_LEN = 100
   integer, parameter :: REAL_KIND = 8
   character(len=*), parameter :: DEFAULT_PATH = "../run/config.txt"
 contains
+  integer function bool2int(val)
+    logical, intent(in) :: val
+    if (val) then
+      bool2int = 1
+    else
+      bool2int = 0
+    end if
+  end function bool2int
+
   function get_path() result(path)
     implicit none
     integer :: ios, num_args
@@ -36,22 +46,34 @@ contains
 
   subroutine print_arr_1d(arr, name)
     implicit none
-    real(kind=REAL_KIND), intent(in) :: arr(:, :)
+    real(kind=REAL_KIND), intent(in) :: arr(:)
     character(len=*), intent(in), optional :: name
+
+    integer :: i
+
     if(present(name)) then
       print *, name
     end if
-    print *, arr
+    do i=1, size(arr)
+      print *, arr(i)
+    end do
   end subroutine print_arr_1d
 
   subroutine print_arr_2d(arr, name)
     implicit none
     real(kind=REAL_KIND), intent(in) :: arr(:, :)
     character(len=*), intent(in), optional :: name
+
+    integer :: arr_shape(2)
+    integer :: i
+    arr_shape = shape(arr)
+
     if(present(name)) then
       print *, name
     end if
-    print *, arr
+    do i=1, arr_shape(2)
+      print *, arr(:, i)
+    end do
   end subroutine
 
 !  integer function find_line_number(unit, search_str) result(line_num)
@@ -73,9 +95,15 @@ contains
 !    end do
 !  end function find_line_number
 
+  ! File processing methods
+
   integer function get_num_lines(path) result(n)
     ! Based on
     ! https://stackoverflow.com/questions/30692424/how-to-read-number-of-lines-in-fortran-90-from-a-text-file
+    ! Return codes:
+    ! > 0 = number of lines
+    ! -1 = could not open file
+    ! -2 = could not read a line in the file
     implicit none
     character(len=*), intent(in) :: path
     integer :: ios
@@ -86,10 +114,16 @@ contains
       return
     end if
     do
-      read(FILE_UNIT, *, end=10)
+      read(FILE_UNIT, *, iostat=ios)
+      if (ios > 0) then
+        n = -2
+        return
+      else if (ios < 0) then
+        exit
+      end if
       n = n + 1
     end do
-    10 close(FILE_UNIT)
+    close(FILE_UNIT)
   end function get_num_lines
 
   integer function read_file_to_arr(path, file) result(ret)
@@ -115,30 +149,16 @@ contains
     close(FILE_UNIT)
   end function read_file_to_arr
 
-!  function read_arg_real(unit, name) result(arg)
-!    implicit none
-!    integer, intent(in) :: unit
-!
-!  end function read_arg_real
-!
-!  function read_arg_int(unit, name) result(arg)
-!
-!  end function read_arg_int
-
-  subroutine read_file(path, x, v, m, dt, n_steps, n_objs)
+  subroutine read_file(path, x, v, m, dt, n_steps, n_objs, print_interval)
     implicit none
     character(len=*), intent(in) :: path
-    integer, intent(out) :: n_objs, n_steps
+    integer, intent(out) :: n_objs, n_steps, print_interval
     real(kind=REAL_KIND), allocatable, intent(out) :: m(:), x(:, :), v(:, :)
     real(kind=REAL_KIND), intent(out) :: dt
 
-    character(len=MAX_LINE_LEN) line
-    integer :: ios, n_lines
-    n_lines = get_num_lines(path)
-    if (n_lines < 0) then
-      print *, "Could not open config file :", path
-      stop
-    end if
+    real(kind=REAL_KIND) :: t
+    character(len=MAX_LINE_LEN) line, trimmed, name, value, current_array
+    integer :: ios, i_line, arrays_started, scalars_started, delim_pos, arrays_allocated, array_index
 
     open(unit=FILE_UNIT, file=path, status="old", iostat=ios)
     if (ios /= 0) then
@@ -146,17 +166,126 @@ contains
       stop
     end if
 
+    ! Header check
     read(FILE_UNIT, "(a)", iostat=ios) line
     if (ios /= 0 .or. line /= "planetary-motion configuration") then
       print *, "Invalid header in file: ", path
       stop
     end if
 
-    read(FILE_UNIT, "(a)", iostat=ios) line
-    if (ios /= 0 .or. line /= "n_objs") then
+    ! Actual reading
+    t = -1
+    dt = -1
+    n_objs = -1
+    n_steps = -1
+    print_interval = 0
 
+    i_line = 0
+    scalars_started = 0
+    arrays_started = 0
+    arrays_allocated = 0
+    current_array = ""
+    array_index = 0
+    do
+      i_line = i_line + 1
+      read(FILE_UNIT, "(a)", iostat=ios) line
+      ! Check for errors and end of file
+      if (ios > 0) then
+        print *,"Could not read line ", i_line
+      else if (ios < 0) then
+        exit
+      end if
+      ! Skip comments and empty lines
+      ! Note that trimming an empty line does not result in a string of length 0,
+      ! but an empty string does equal " "
+      trimmed = trim(line)
+      if(trimmed == " " .or. trimmed(1:1) == "#" .or. trimmed(1:1) == ";") then
+        cycle
+      end if
+
+      ! Process the line contents
+      if (trimmed == "[scalars]") then
+        if (arrays_started == 1) then
+          print *,"Cannot start scalars twice"
+          stop
+        end if
+        scalars_started = 1
+        arrays_started = 0
+      else if (trimmed == "[arrays]") then
+        if (arrays_started == 1) then
+          print *,"Cannot start arrays twice"
+          stop
+        end if
+        arrays_started = 1
+        scalars_started = 0
+      ! Scalar processing
+      else if (scalars_started == 1) then
+        delim_pos = scan(trimmed, "=")
+        if(delim_pos < 1) then
+          print *, "Could not parse delimiter on scalar configuration line: ", line
+          stop
+        end if
+        name = trim(trimmed(1:delim_pos-1))
+        value = trim(trimmed(delim_pos+1:))
+        if (name == "n_objs") then
+          read(value, *, iostat=ios) n_objs
+        else if (name == "n_steps") then
+          read(value, *, iostat=ios) n_steps
+        else if (name == "dt") then
+          read(value, *, iostat=ios) dt
+        else if (name == "t") then
+          read(value, *, iostat=ios) t
+        end if
+        if (ios /= 0) then
+          print *, "Could not convert value to correct type on configuration line: ", line
+        end if
+      ! Array processing
+      else if (arrays_started == 1) then
+        ! Allocate memory if not yet allocated
+        if (n_objs < 1) then
+          print *, "n_objs must be specified before starting arrays for proper memory allocation"
+          stop
+        else if (arrays_allocated == 0) then
+          allocate(x(DIMS, n_objs), v(DIMS, n_objs), m(n_objs))
+          arrays_allocated = 1
+        end if
+
+        if (current_array == " ") then
+          current_array = trimmed
+          array_index = 0
+          ! print *, "processing array ", current_array
+        else
+          array_index = array_index + 1
+
+          if (current_array == "x") then
+            read(trimmed, *, iostat=ios) x(:, array_index)
+          else if (current_array == "v") then
+            read(trimmed, *, iostat=ios) v(:, array_index)
+          else if (current_array == "m") then
+            read(trimmed, *, iostat=ios) m(array_index)
+          end if
+          if (ios /= 0) then
+            print *, "Could not put the line '", line, "' into array ", current_array
+          end if
+
+          if (array_index == n_objs) then
+            current_array = ""
+          end if
+        end if
+      end if
+    end do
+    close(FILE_UNIT)
+
+    ! Post-processing and checks
+    if (bool2int(dt > 0) + bool2int(t > 0) + bool2int(n_steps > 0) /= 2) then
+      print *, "Exactly two of td, t and n_steps must be configured"
+      stop
+    end if
+    if (dt < 0) then
+      dt = t / n_steps
+    else if (n_steps < 0) then
+      n_steps = nint(t / dt)
     end if
 
-    close(FILE_UNIT)
   end subroutine read_file
 end module utils
